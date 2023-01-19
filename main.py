3816a967418a4dd416cc63e1ade4cece6deab974
Student <andrewhamers@dpengineering.org>
2023-01-19 14:09:38 -0800

    new Rpi aquired

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ COUNTERCLOCKWISE = 1
 ARM_SLEEP = 2.5
 DEBOUNCE = 0.10
 
-lowerTowerPosition = 54  # position of the lower tower
-upperTowerPosition = 32  # position of the upper tower
+lowerTowerPosition = 32  # position of the lower tower
+upperTowerPosition = 54  # position of the upper tower
 
 
 # ////////////////////////////////////////////////////////////////
